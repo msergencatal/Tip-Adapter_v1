c19d37f0deea67dc13471d1d97a8c6a4711f808b
msergencatal <61971321+msergencatal@users.noreply.github.com>
2023-11-26 20:37:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -97,8 +97,8 @@ def run_tip_adapter(cfg, cache_keys, cache_values, val_features, val_labels, tes
     print("**** Tip-Adapter's test accuracy: {:.2f}. ****\n".format(acc))
 
 # function definition of 'run_tip_adapter_F()'
-# clip model-->
-# train_loader_F-->
+# clip model--> pre-trained CLIP model
+# train_loader_F--> 
 def run_tip_adapter_F(cfg, cache_keys, cache_values, val_features, val_labels, test_features, test_labels, clip_weights, clip_model, train_loader_F):
     
     # Enable the cached keys to be learnable
